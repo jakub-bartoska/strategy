4cee8192a8edab0d2c4c6a72ecc431c0f7a9023b
Eleerek <jakub.bartoska@ematiq.com>
2023-11-14 21:53:42 +0100

    fix buildings display

--- Assets/scripts/_Monobehaviors/ui/strategy/town-buildings-ui/BuildingDisplayer.cs
+++ Assets/scripts/_Monobehaviors/ui/strategy/town-buildings-ui/BuildingDisplayer.cs
@@ -13,7 +13,7 @@ namespace _Monobehaviors.town_buildings_ui
 
         [SerializeField] public Transform buildingParent;
         public GameObject buildingPrefab;
-        public List<(BuildingType, GameObject, bool)> existingTabs = new();
+        public List<(BuildingType, GameObject, bool)> existingTabs = new(20);
 
         public void Awake()
         {
@@ -22,12 +22,27 @@ namespace _Monobehaviors.town_buildings_ui
 
         public void displayBuilding(BuildingType buildingType, NativeList<ResourceHolder> resources, bool hasTownBuilding)
         {
-            if (alreadyExists(buildingType, hasTownBuilding)) return;
+            var tabExists = alreadyExists(buildingType);
 
+            if (tabExists)
+            {
+                if (hasDifferentData(buildingType, hasTownBuilding))
+                {
+                    updateTab(buildingType, resources, hasTownBuilding);
+                }
+
+                return;
+            }
+
+            createTab(buildingType, resources, hasTownBuilding, existingTabs.Count);
+        }
+
+        private void createTab(BuildingType buildingType, NativeList<ResourceHolder> resources, bool hasTownBuilding, int index)
+        {
             var newInstance = Instantiate(buildingPrefab, buildingParent);
             newInstance.GetComponent<BuildingPrefabData>().setBuildingType(buildingType, hasTownBuilding);
             var rectTransform = newInstance.GetComponent<RectTransform>();
-            var y = -100 * existingTabs.Count - 50;
+            var y = -100 * index - 50;
             rectTransform.anchoredPosition = new Vector3(150, y);
             if (hasTownBuilding)
             {
@@ -40,17 +55,44 @@ namespace _Monobehaviors.town_buildings_ui
                 buildingResourceRowManager.addResource(row);
             }
 
-            existingTabs.Add((buildingType, newInstance, hasTownBuilding));
+            if (index == existingTabs.Count)
+                existingTabs.Add((buildingType, newInstance, hasTownBuilding));
+            else
+                existingTabs[index] = (buildingType, newInstance, hasTownBuilding);
         }
 
-        private bool alreadyExists(BuildingType buildingType, bool hasTownBuilding)
+        private bool alreadyExists(BuildingType buildingType)
         {
             foreach (var tab in existingTabs)
             {
-                if (tab.Item1 == buildingType && tab.Item3 == hasTownBuilding) return true;
+                if (tab.Item1 == buildingType) return true;
             }
 
             return false;
+        }
+
+        private bool hasDifferentData(BuildingType buildingType, bool hasTownBuilding)
+        {
+            var tab = existingTabs.Find(x => x.Item1 == buildingType);
+
+            return tab.Item1 != buildingType || tab.Item3 != hasTownBuilding;
+        }
+
+        private void updateTab(BuildingType buildingType, NativeList<ResourceHolder> resources, bool hasTownBuilding)
+        {
+            var tabIndex = 0;
+            for (int i = 0; i < existingTabs.Count; i++)
+            {
+                if (existingTabs[i].Item1 == buildingType)
+                {
+                    tabIndex = i;
+                    break;
+                }
+            }
+
+            var tab = existingTabs[tabIndex];
+            Destroy(tab.Item2);
+            createTab(buildingType, resources, hasTownBuilding, tabIndex);
         }
     }
 }
