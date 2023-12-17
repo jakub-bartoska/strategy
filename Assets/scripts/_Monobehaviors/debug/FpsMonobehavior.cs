c3c283c5d19dad7405dc2dd4d8b36e67885110f1
Eleerek <jakub.bartoska@ematiq.com>
2023-12-17 20:24:21 +0100

    add fps indicator

---
+++ Assets/scripts/_Monobehaviors/debug/FpsMonobehavior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace _Monobehaviors.debug
+{
+    public class FpsMonobehavior : MonoBehaviour
+    {
+        public static FpsMonobehavior instance;
+
+        [SerializeField] private TextMeshProUGUI text;
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        public void updateFps(int fps)
+        {
+            text.text = fps.ToString();
+        }
+    }
+}
---
+++ Assets/scripts/system/_common/debug/FpsSystem.cs
@@ -0,0 +1,18 @@
+﻿using _Monobehaviors.debug;
+using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+namespace system._common.debug
+{
+    public partial struct FpsSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+            float fps = 1.0f / deltaTime;
+            FpsMonobehavior.instance.updateFps((int) fps);
+        }
+    }
+}
